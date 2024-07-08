45fd68c63e6486c4680a990d14c16e9dbfa3185e
Boxo-se <sperasspelarskit@gmail.com>
2024-07-09 00:14:25 +0200

    Add files via upload

---
+++ CFGL.bat
@@ -0,0 +1,50 @@
+@echo OFF
+color 03
+title APB CFG LOADER
+ECHO _________________________________________________________________________________
+ECHO Welcome to Box's Config loader. Go read the readme.txt! 
+Echo Normal config :1
+Echo No stutter config :2
+Echo No stutter config + UI :3
+ECHO _________________________________________________________________________________
+set /p load=
+if %load%==1 (
+echo Patching normal config
+xcopy "enter file path to boxcfg"\cfg\Box_CFG1.6 "enter file path to steam common" /s /e /u /r /y /q
+)
+if %load%==2 (
+echo Patching no stutter config
+xcopy "enter file path to boxcfg"\Box_CFG1.6stutter "enter file path to steam common" /s /e /u /r /y /q
+)
+if %load%==3 (
+echo Patching no stutter config
+xcopy "enter file path to boxcfg"\Box_CFG1.6UI "enter file path to steam common" /s /e /u /r /y /q
+)
+
+echo all OK
+color 02
+
+ECHO _________________________________________________________________________________
+ECHO Time to launch
+Echo Launch with steam :1
+Echo Launch without steam :2
+ECHO _________________________________________________________________________________
+
+set /p str=
+if %str%==1 (
+echo Launching with steam
+start "enter path to apb.lnk with the launch options you want"
+)
+if %str%==2 (
+echo Launching without steam
+start "enter path to apb.lnk with the launch options you want"
+)
+
+
+
+
+ECHO _________________________________________________________________________________
+ECHO Starting APB
+ECHO _________________________________________________________________________________
+
+timeout /t 5 /nobreak
---
+++ readme.txt
@@ -0,0 +1,26 @@
+Box's config 1.3
+
+I will not take any responsability for anyone that uses this config.
+Use on your own discretion
+
+QnA
+
+Will I make any changes to this?
+No.
+
+How do you apply the config? 
+Drag and drop the files in x\Steam\steamapps\common or use the config loader included. 
+
+What does CFGL.bat do?
+Since I use 3 different configs I've made a bat file to switch between them quickly. 
+All it does is copy the files from destination x to y. You can use it by chaning the path ways.
+https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/xcopy
+
+Will you answer questions about the config or the config loader.
+No, I dont intend to maintane this or share newer configs. This is only to share the little I have.
+It is my variant of Iazers config which you can find at https://github.com/lvzxr/apb-reloaded
+
+
+Box_CFG1.3 = GC on
+Box_CFG1.3stutter = GC off
+Box_CFG1.3UI = GC off - UI parts
